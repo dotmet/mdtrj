111b1bd43e0972e8322257ace5f153eba84ef833
Gddr100x <77543771@qq.com>
2023-08-26 16:00:54 +0800

    .

--- docs/conf.py
+++ docs/conf.py
@@ -6,6 +6,12 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
+import os
+import sys
+import sphinx
+
+sys.path.insert(0, os.path.abspath('../'))
+
 project = 'mdtrj'
 copyright = '2023, Even Wong'
 author = 'Even Wong'
